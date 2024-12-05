548e3d18b5c9658db64cf983102b3e561733e93c
tainur <tainurrahaman31@gmail.com>
2024-12-05 15:48:18 +0600

    add more components for functionality

---
+++ src/firebase_init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDf5yxOFaKrOn2p8EGbP7XBJMPalmtPjOk",
+  authDomain: "login-registration-auth-8955a.firebaseapp.com",
+  projectId: "login-registration-auth-8955a",
+  storageBucket: "login-registration-auth-8955a.firebasestorage.app",
+  messagingSenderId: "890032765614",
+  appId: "1:890032765614:web:0091cb82aca35e18cb0b3f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,29 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/registration",
+        element: <Registration></Registration>,
+      },
+    ],
   },
 ]);
 
